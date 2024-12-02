d59347d98e23dd4087932efa178ea47b83b6c2a8
safakselimsofioglu <154444832+safakselimsofioglu@users.noreply.github.com>
2024-12-02 20:07:17 +0300

    Add files via upload

---
+++ SwiftUI Basics/SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Şafak Selim Sofioğlu on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
