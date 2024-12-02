4bfda00c8cdbd252d8653d4662d5b9ae988abc02
safakselimsofioglu <154444832+safakselimsofioglu@users.noreply.github.com>
2024-12-02 20:09:20 +0300

    Add files via upload

---
+++ 02-SwiftUI-Basics/Animate Fonts/Animate Fonts/Animate_FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_FontsApp.swift
+//  Animate Fonts
+//
+//  Created by Şafak Selim Sofioğlu on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Animate_FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 02-SwiftUI-Basics/Animate Fonts/Animate Fonts/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Animate Fonts
+//
+//  Created by Şafak Selim Sofioğlu on 2.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
